86c26a63c5ee39e12eea7007f660da318c8899df
Jose Eliecer Rivera Perez <jose.rivera71@inacapmail.cl>
2025-10-29 22:47:39 -0300

    comid de comit

---
+++ gestionApp/__init__.py
@@ -0,0 +1,11 @@
+from gestionApp.forms import TensForm
+from medicoApp.forms import MedicoForm
+from matronaApp.forms import MatronaForm
+
+__all__ = [
+    'PersonaForm',
+    'PacienteForm',
+    'MedicoForm',
+    'MatronaForm',
+    'TensForm',
+]
